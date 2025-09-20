481c3b61b5b59e277565a47aa062c27fa488359a
CaptainCheetah978 <84174958+CaptainCheetah978@users.noreply.github.com>
2025-09-20 08:30:24 +0530

    Create secrets.example.h
    This file can be used when you need to create a secure data transmission through WiFi hence they should be kept secertly.

---
+++ secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+// Copy this to include/secrets.h and edit. DO NOT COMMIT secrets.h
+
+const char* WIFI_SSID     = "YOUR_WIFI_SSID";
+const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+const char* GAS_URL       = "https://script.google.com/macros/s/XXXXX/exec";
+const char* SECRET_TOKEN  = "REPLACE_WITH_A_LONG_RANDOM_TOKEN";
